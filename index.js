7afb0fa46625a72814ef801fe446a5a5a1829972
mattdesl <dave.des@gmail.com>
2015-06-08 10:07:33 -0400

    constructor should not be enumerable

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function inherits (ctor, superCtor) {
   ctor.prototype = Object.create(superCtor.prototype, {
     constructor: {
       value: ctor,
-      enumerable: true,
+      enumerable: false,
       writable: true,
       configurable: true
     }
